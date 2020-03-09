bdbfe66e8fd2a94c8c607f17689b83e39da99c94
Haris Beslic <harisbeslic@Hariss-MacBook-Pro.local>
2020-03-09 10:53:00 +0100

    removed unused imports

--- src/__tests__/components/gridItem.test.tsx
+++ src/__tests__/components/gridItem.test.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { BrowserRouter, Link } from 'react-router-dom';
 import { shallow } from 'enzyme';
 
 import GridItem from 'components/gridItem/gridItem';
